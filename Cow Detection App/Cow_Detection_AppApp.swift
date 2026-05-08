5edeed503102086c6fabc9fd6d71dd97b6803c87
Adii78560 <raiaditya145@gmail.com>
2026-05-08 23:23:17 +0530

    Initial Commit

---
+++ Cow Detection App/Cow_Detection_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cow_Detection_AppApp.swift
+//  Cow Detection App
+//
+//  Created by Aditya Rai on 08/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Cow_Detection_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
